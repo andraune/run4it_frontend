dceabe3ca50a12058b42d8fe9e3ca06880e7de16
Andreas MacMini1 <andreas.aune@gmail.com>
2020-02-02 14:44:26 +0100

    Added version to footer. Moved manifest.

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+
+import { ApiService } from "../api-common";
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: [':host { display: flex;flex-wrap: wrap;width: 100%;']
+})
+export class FooterComponent implements OnInit {
+
+  apiVersion: number;
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.get("").subscribe(apiData => this.apiVersion = apiData.version);
+  }
+
+}
