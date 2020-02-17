7da7b2d386f938ea816876d510f5234de1797d51
Andreas MacMini1 <andreas.aune@gmail.com>
2020-02-17 15:48:12 +0100

    Formatting page-not-found

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styles: [':host { display: flex;flex-wrap: wrap;width: 100%;']
+  styles: [':host { display: flex; width: 100%;']
 })
 export class PageNotFoundComponent implements OnInit {
 
