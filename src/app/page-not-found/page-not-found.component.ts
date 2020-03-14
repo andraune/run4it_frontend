c20116ec2f91cfd4fa0535608b0f3038f668d62e
Andreas MacMini1 <andreas.aune@gmail.com>
2020-03-14 15:44:10 +0100

    Move styling from html to css

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styles: [':host { display: flex; width: 100%;']
+  styles: [
+    ':host { display: flex; width: 100%; }',
+    'div.ooops { width:100%; text-align:center; margin-top:50px; }'
+  ]
 })
 export class PageNotFoundComponent implements OnInit {
 
